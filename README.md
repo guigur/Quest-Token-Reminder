c27fba567d6765d4f63274495831c10417e454fb
guigur <guigur13@gmail.com>
2019-11-27 16:51:34 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Quest-Tocken-Reminder
+
+1 Create an slack app
+2 Retrieve your tockens
+3 Copy paste the settings.json.sample and rename it settings.json
+4 Fill this file with you slack tocken, the url of the quest tracking tool and the choosen channel
+5 Fill the codes.json with entries
+6 Create a cron entry to execute this program multiples times a day
+ex: 30 5 * * * python3 /path/to/qtr.py
+
+Voilà
---
+++ qtr.py
@@ -0,0 +1,31 @@
+import time
+import re
+import slack
+import datetime
+import json
+
+message = ""
+settings = ""
+with open('settings.json') as json_file:
+    settings = json.load(json_file)
+
+web_client = slack.WebClient(settings['tocken'])
+
+today = datetime.date.today()
+print("Today's date:", today)
+format_str = '%d-%m-%Y'
+
+with open('codes.json') as json_file:
+    data = json.load(json_file)
+    for entry in data['quest']:
+        dateStart  = datetime.datetime.strptime(entry['start'], format_str).date()
+        dateEnd  = datetime.datetime.strptime(entry['end'], format_str).date()
+        if ((today >= dateStart) and (today <= dateEnd)):
+            message = "Le code quest d'aujoud'hui : " + entry['code'] + "\r\n pour le module : " + entry['module'] + "\r\n" + settings['urlQuest']
+            response = web_client.chat_postMessage(channel=settings['channel'], text=message)
+            if (response['ok'] == True):
+                print("ok")
+            else:
+                print("prblm avec l'envoi")
+            print(message)
+
